570609ba9dc6d28ed6bf084da4e2e15420967753
kelifire <mom005@mymail.tc3.edu>
2023-02-07 13:47:51 -0500

    Comment and organize main

--- src/main.c
+++ src/main.c
@@ -1,35 +1,73 @@
 //=========================================//
 //   Title:  main.c                        //
 //  Author:  Miguel                        //
-//    Date:  12/31/2022                    //
-// Version:  1                             //
+//    Date:  02/07/2023                    //
+// Version:  2                             //
 //=========================================//
-#include <rpi.h>
+#include <gpio.h>
+#include <pwm.h>
+#include <uart.h>
 
-void main(){
+void main() {
+
+    // initialize UART with 115200Bd (Baud)
     uart_init(115200);
     uart_puts("UART Initialized\r\n");
+
+    // initialize Pulse Width Modulation (PWM) with freq=9.6MHz range=255
     pwm_init(9.6, 255,
         PWM_MSEN2|PWM_MSEN1|PWM_PWEN2|PWM_PWEN1,
         CLK_MASH(0)|CLK_SRC_OSCI);
+
     uart_puts("PWM Initialized\r\n");
 
-    fsel(12, FSEL_ALT0); // pwm channel 1
-    fsel(13, FSEL_ALT0); // pwm channel 2
-    char dat1 = 0;
-    char dir1 = 1;
-    char dat2 = 127;
-    char dir2 = -1;
-    while(1) {
+    fsel(12, FSEL_ALT0);
+    uart_puts("GPIO pin 12 set to ALT0 (PWM Channel 1)\r\n");
+    fsel(21, FSEL_OUT);
+    uart_puts("GPIO pin 21 set to Output\r\n");
+    fsel(26, FSEL_IN);
+    uart_puts("GPIO pin 26 set to Input\r\n");
+    write(GPREN0, 1<<26);
+    uart_puts("Rising Edge Detection enabled on GPIO pin 26\r\n");
+
+    // Variables
+    char dat = 0;   // Used for PWM value, min=0 max=255
+    bool pin21_state = false; // Keep track of the state of pin 21
+    pireg32_t gpeds0_register = 0; // Varibale to store reads from Event Detect Register
+    nanos timenow = read(BCM_TIMER); // Timer
+    nanos timeprev = timenow;        // Timer
+
+    // main Loop
+    while(1)
+    {
+        // default delay of each loop
         sleep(2*MILLIS_10);
 
-        pwm_wdat(1, dat1);
-        pwm_wdat(2, dat2);
-        dat1 += dir1;
-        dat2 += dir2;
-        if (dat1 == 127 || dat1 == 0)
-            dir1 *= -1;
-        if (dat2 == 127 || dat2 == 0)
-            dir2 *= -1;
+        // toggle pin 21 every 1 second
+        timenow = read(BCM_TIMER);
+        if ((timenow - timeprev) > SECONDS_1)
+        {
+            timeprev = timenow;
+            if (pin21_state)
+                gpio_set(21);
+            else
+                gpio_clr(21);
+            pin21_state = !pin21_state;
+        }
+
+
+        // check if button was pressed
+        gpeds0_register = read(GPEDS0);     // read the Event Detect Register (GPIO pins 0-31)
+        if ((gpeds0_register >> 26) & 0x1)  // isolate pin 26 bit position (check GPIO 26 activitiy)
+        {
+            uart_puts("Button 26 pressed");
+            write(GPEDS0, 0xffffffff);      // clean EDS register
+        }
+
+        // gradually increase value of PWM on Channel 1
+        pwm_set(1, dat);
+        dat += 1;
+        if (dat == 255) // Reset when it reaches maximum value (should overflow anyway)
+            dat = 0;
     }
 }
